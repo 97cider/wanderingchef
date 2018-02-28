87913019329d94e797aa772670ab06995012183f
bmm349 <bmm349@drexel.edu>
2018-02-27 22:00:24 -0500

    Mobile Implementation of gMove

--- Assets/PlayerController/fMovement/fMovement.cs
+++ Assets/PlayerController/fMovement/fMovement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class fMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    public GameObject Ground;
+    public Quaternion diffAng, playerAngle;
+
+    public void Start() {
+        diffAng = Ground.transform.rotation;
+        GameObject player = this.gameObject;
+        playerAngle = player.transform.rotation;
+    }
 	void Update () {
-		
-	}
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var y = diffAng.eulerAngles - playerAngle.eulerAngles;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        Debug.Log("Player EulerY" + playerAngle.eulerAngles.y);
+
+        transform.Rotate(diffAng.eulerAngles - playerAngle.eulerAngles);
+        transform.Translate(x, y.y, z);
+    }
 }
