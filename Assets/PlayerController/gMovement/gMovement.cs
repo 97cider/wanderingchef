d26e9d401fe466d53b60330195272e3cdb831ca5
bmm349 <bmm349@drexel.edu>
2018-02-27 22:00:24 -0500

    Mobile Implementation of gMove

--- Assets/PlayerController/gMovement/gMovement.cs
+++ Assets/PlayerController/gMovement/gMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class gMovement : MonoBehaviour {
 
@@ -61,59 +62,61 @@ public class gMovement : MonoBehaviour {
         }
     }
    
+    public void MoveNorth()
+    {
+        if (checkNorth(currentTile))
+        {
+            Debug.Log("Player can move to the north");
+            player.transform.position = currentTile.GetComponent<tileLayout>().nTile.transform.position + new Vector3(0, 1.3f, 0);
+            currentTile = currentTile.GetComponent<tileLayout>().nTile;
+        }
+        else
+        {
+            Debug.Log("Player cannot move to the north");
+        }
+    }
+    public void MoveSouth()
+    {
+        if (checkSouth(currentTile))
+        {
+            Debug.Log("Player can move to the south");
+            player.transform.position = currentTile.GetComponent<tileLayout>().sTile.transform.position + new Vector3(0, 1.3f, 0);
+            currentTile = currentTile.GetComponent<tileLayout>().sTile;
+        }
+        else
+        {
+            Debug.Log("Player cannot move to the sout");
+        }
+    }
+
+    public void MoveWest()
+    {
+        if (checkWest(currentTile))
+        {
+            Debug.Log("Player can move to the west");
+            player.transform.position = currentTile.GetComponent<tileLayout>().wTile.transform.position + new Vector3(0, 1.3f, 0);
+            currentTile = currentTile.GetComponent<tileLayout>().wTile;
+        }
+        else
+        {
+            Debug.Log("Player cannot move to the west");
+        }
+    }
+
+    public void MoveEast() {
+        if (checkEast(currentTile))
+        {
+            Debug.Log("Player can move to the east");
+            player.transform.position = currentTile.GetComponent<tileLayout>().eTile.transform.position + new Vector3(0, 1.3f, 0);
+            currentTile = currentTile.GetComponent<tileLayout>().eTile;
+        }
+        else
+        {
+            Debug.Log("Player cannot move to the east");
+        }
+    }
     void Update()
     {
-        if (Input.GetKeyDown("w"))
-        {
-            if (checkNorth(currentTile))
-            {
-                Debug.Log("Player can move to the north");
-                player.transform.position = currentTile.GetComponent<tileLayout>().nTile.transform.position + new Vector3(0, 1.3f, 0);
-                currentTile = currentTile.GetComponent<tileLayout>().nTile;
-            }
-            else
-            {
-                Debug.Log("Player cannot move to the north");
-            }
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            if (checkSouth(currentTile))
-            {
-                Debug.Log("Player can move to the south");
-                player.transform.position = currentTile.GetComponent<tileLayout>().sTile.transform.position + new Vector3(0, 1.3f, 0);
-                currentTile = currentTile.GetComponent<tileLayout>().sTile;
-            }
-            else
-            {
-                Debug.Log("Player cannot move to the sout");
-            }
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            if (checkWest(currentTile))
-            {
-                Debug.Log("Player can move to the west");
-                player.transform.position = currentTile.GetComponent<tileLayout>().wTile.transform.position + new Vector3(0, 1.3f, 0);
-                currentTile = currentTile.GetComponent<tileLayout>().wTile;
-            }
-            else
-            {
-                Debug.Log("Player cannot move to the west");
-            }
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            if (checkEast(currentTile))
-            {
-                Debug.Log("Player can move to the east");
-                player.transform.position = currentTile.GetComponent<tileLayout>().eTile.transform.position + new Vector3(0, 1.3f, 0);
-                currentTile = currentTile.GetComponent<tileLayout>().eTile;
-            }
-            else
-            {
-                Debug.Log("Player cannot move to the east");
-            }
-        }
+        Vector2 joystickPos = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
     }
 }
